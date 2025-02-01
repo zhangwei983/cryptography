130742a525e8ec74b030824ddb63d575c98f8af0
Vincent Zhang <zhangwei983@hotmail.com>
2025-02-01 17:52:11 +0800

    Add a sample for in-place encryption and decryption.

--- test_aes_gcm/src/main.rs
+++ test_aes_gcm/src/main.rs
@@ -1,15 +1,16 @@
 use aes_gcm::{
-    aead::{Aead, AeadCore, OsRng},
+    aead::{heapless::Vec, Aead, AeadCore, AeadInPlace, OsRng},
     Aes256Gcm, KeyInit,
 };
 
 fn main() {
     let key = Aes256Gcm::generate_key(OsRng);
-
     let cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
     let message = b"plaintext message";
+
+    // Normal encryption/decryption.
     let ciphertext = cipher
         .encrypt(&nonce, message.as_ref())
         .expect("Failed to encrypt.");
@@ -22,5 +23,20 @@ fn main() {
     println!(
         "Decrypted message: {:?}",
         String::from_utf8(plaintext.to_vec()).unwrap()
+    );
+
+    // In-place encryption/decryption.
+    let mut buffer: Vec<u8, 128> = Vec::new();
+    buffer.extend_from_slice(message).ok();
+
+    cipher.encrypt_in_place(&nonce, b"", &mut buffer).unwrap();
+    assert_ne!(&buffer, message);
+
+    cipher.decrypt_in_place(&nonce, b"", &mut buffer).unwrap();
+    assert_eq!(buffer, message);
+
+    println!(
+        "Decrypted message: {:?}",
+        String::from_utf8(buffer.to_vec()).unwrap()
     );
 }
