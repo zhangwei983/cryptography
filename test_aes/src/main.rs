4907af4fd106be3e109e07a01dea05d88bd149ac
Vincent Zhang <zhangwei983@hotmail.com>
2025-02-01 17:27:15 +0800

    Add a simple sample for aes library.

---
+++ test_aes/src/main.rs
@@ -0,0 +1,27 @@
+use aes::Aes128;
+use aes::cipher::{
+    BlockEncrypt, BlockDecrypt, KeyInit, generic_array::GenericArray
+};
+
+fn main() {
+    // Generate a random 128-bit key.
+    let key = GenericArray::from([0u8; 16]);
+    println!("{}", hex::encode(key.as_slice()));
+
+    // Initialize block to all 42s.
+    let mut block = GenericArray::from([42u8; 16]);
+    println!("{}", hex::encode(block.as_slice()));
+    
+    // Create an AES128 cipher with the key.
+    let cipher = Aes128::new(&key);
+    let block_copy = block.clone();
+
+    // Encrypt block in-place.
+    cipher.encrypt_block(&mut block);
+    println!("{}", hex::encode(block.as_slice()));
+
+    // Decrypt block in-place.
+    cipher.decrypt_block(&mut block);
+
+    assert_eq!(block, block_copy);
+}
