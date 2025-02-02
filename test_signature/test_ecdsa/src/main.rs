baa441808c21c140d629bccf9fa1eec8dacbf88e
Vincent Zhang <zhangwei983@hotmail.com>
2025-02-02 16:22:26 +0800

    Refactor the code a little bit.

--- test_signature/test_ecdsa/src/main.rs
+++ test_signature/test_ecdsa/src/main.rs
@@ -2,43 +2,21 @@ use k256::ecdsa::{signature::Signer, signature::Verifier, Signature, SigningKey,
 use rand_core::OsRng;
 
 fn main() {
-    // Prepare the message to sign.
-    let message = String::from("Hello world");
-    let msg = message.as_bytes();
-
     // Generate the signing key.
     let signing_key = SigningKey::random(&mut OsRng);
-    let sk = signing_key.to_bytes();
-    println!("Signing key: {:x?}", hex::encode(sk));
 
-    // Sign with the signing key.
-    let signature: Signature = signing_key.sign(msg);
+    // Sign.
+    let message = b"Hello world";
+    let signature: Signature = signing_key.sign(message);
     println!("Signature: {:x?}", hex::encode(signature.to_bytes()));
 
-    // Generate the verifying key.
+    // Verify
     let verify_key = VerifyingKey::from(&signing_key);
-    let vk = verify_key.to_sec1_bytes();
-    println!("Verifying key: {:x?}", hex::encode(vk));
-
-    // Verify with the verifying key.
-    let verified = verify_key.verify(msg, &signature).is_ok();
-
-    if verified {
-        println!("'{}' signature matches", message);
-    } else {
-        println!("'{}' signature mismatches", message);
-    }
+    let result = verify_key.verify(message, &signature);
+    println!("Result: {:?}", result.is_ok());
 
     // Prepare the wrong message.
-    let message = String::from("Hello");
-
-    let msg = message.as_bytes();
-
-    let verified = verify_key.verify(msg, &signature).is_ok();
-
-    if verified {
-        println!("'{}' signature matches", message);
-    } else {
-        println!("'{}' signature mismatches", message);
-    }
+    let message = b"Hello";
+    let result = verify_key.verify(message, &signature);
+    println!("Result: {:?}", result.is_ok());
 }
