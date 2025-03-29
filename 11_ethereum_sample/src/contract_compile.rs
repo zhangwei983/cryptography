f081d8d53977f4a4f4072c7377a2938bd6dcee63
Vincent Zhang <zhangwei983@hotmail.com>
2025-03-29 22:56:03 +0800

    Small polishment.

--- 11_ethereum_sample/src/contract_compile.rs
+++ 11_ethereum_sample/src/contract_compile.rs
@@ -7,7 +7,13 @@ pub async fn test() -> Result<(), Box<dyn std::error::Error>> {
     let contract_name = "SimpleStorage";
     let contract_path = "./contracts/contract.sol";
 
-    let compiled_contract = Solc::default().compile(&contract_path)?;
+    // Compile the Solidity contract.
+    // It requires the `solc` binary to be installed and available in the PATH.
+    // https://docs.soliditylang.org/en/latest/installing-solidity.html
+    let compiled_contract = Solc::default().compile_source(&contract_path)?;
+    //println!("Compiled contract: {:?}", compiled_contract.sources.keys());
+
+    // Get the ABI of the contract.
     let abi = compiled_contract
         .get(&contract_path, &contract_name)
         .ok_or(format!("Contract {} not found", contract_name))?
@@ -16,6 +22,7 @@ pub async fn test() -> Result<(), Box<dyn std::error::Error>> {
 
     let abi_json = serde_json::to_string(abi)?;
 
+    // Generate Rust bindings for the contract and write to the stdout.
     let bindings = Abigen::new(&contract_name, abi_json)?.generate()?;
     bindings.write(&mut std::io::stdout())?;
 
